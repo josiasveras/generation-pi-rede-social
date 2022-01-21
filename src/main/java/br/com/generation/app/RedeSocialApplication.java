1d3fbb7bee269e30c9bdb7cafac554ffb510e573
josiasveras <josias_veras@hotmail.com>
2022-01-21 11:29:47 -0300

    Projeto criado

---
+++ src/main/java/br/com/generation/app/RedeSocialApplication.java
@@ -0,0 +1,13 @@
+package br.com.generation.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedeSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedeSocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/generation/app/RedeSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.generation.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedeSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
