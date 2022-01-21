e850aac52c3773a8a3f5d53e3d45c9a93185c4c4
josiasveras <josias_veras@hotmail.com>
2022-01-21 18:10:17 -0300

    C:/Program Files/Git/models/

---
+++ src/main/java/br/com/generation/app/models/Tema.java
@@ -0,0 +1,66 @@
+package br.com.generation.app.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_temas")
+public class Tema {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idTema;
+	
+	@NotBlank
+	@Size(min = 2, max = 100)
+	private String servico;
+	
+	@NotBlank
+	@Size(min = 2, max = 100)
+	private String localizacao;
+	
+	@NotBlank
+	@Size(min = 2, max = 100)
+	private String descricao;
+	
+	public Tema() {
+		super();
+	}
+
+	public Long getIdTema() {
+		return idTema;
+	}
+
+	public void setIdTema(Long idTema) {
+		this.idTema = idTema;
+	}
+
+	public String getServico() {
+		return servico;
+	}
+
+	public void setServico(String servico) {
+		this.servico = servico;
+	}
+
+	public String getLocalizacao() {
+		return localizacao;
+	}
+
+	public void setLocalizacao(String localizacao) {
+		this.localizacao = localizacao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+}
