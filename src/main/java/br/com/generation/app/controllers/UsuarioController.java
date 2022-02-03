055ab08286d21f33d377db56b9dab4caa3a1999e
josiasveras <josias_veras@hotmail.com>
2022-02-03 11:14:17 -0300

    @josiasveras [Task: Atualizar UsuarioController]

--- src/main/java/br/com/generation/app/controllers/UsuarioController.java
+++ src/main/java/br/com/generation/app/controllers/UsuarioController.java
@@ -1,18 +1,25 @@
 package br.com.generation.app.controllers;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.generation.app.models.UserLogin;
 import br.com.generation.app.models.Usuario;
+import br.com.generation.app.repositories.UsuarioRepository;
 import br.com.generation.app.services.UsuarioService;
 
 @RestController
@@ -22,6 +29,9 @@ public class UsuarioController {
 	
 	@Autowired
 	private UsuarioService usuarioService;
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 	
 	@PostMapping("/logar")
 	public ResponseEntity<UserLogin> Autentication(@RequestBody Optional<UserLogin> userLogin){
@@ -34,5 +44,56 @@ public class UsuarioController {
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(usuarioService.cadastrarUsuario(usuario));
 	}
-
+	
+	@GetMapping("/buscar/tudo")
+	public ResponseEntity<List<Usuario>> buscarTodosUsuarios(){
+		List<Usuario> list = usuarioRepository.findAll();
+		
+		if (list.isEmpty()) {
+			return ResponseEntity.status(204).build();
+		} else {
+			return ResponseEntity.status(200).body(list);
+		}
+	}
+	
+	@GetMapping("/buscar/{idUsuario}")
+	public ResponseEntity<Usuario> buscarUsuarioPorId(@PathVariable(value = "idUsuario") Long idUsuario) {
+		return usuarioRepository.findById(idUsuario)
+				.map(resp -> ResponseEntity.status(200).body(resp))
+				.orElseGet(() -> {
+					throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Id não encontrado");
+				});
+	}
+	
+	@GetMapping("/buscar/email/{emailUsuario}")
+	public ResponseEntity<Usuario> buscarUsuarioPorEmail(@PathVariable(value = "emailUsuario") String emailUsuario) {
+		
+		Optional<Usuario> usuario = usuarioRepository.findByEmail(emailUsuario);		
+		if (usuario.isPresent()) {
+			return ResponseEntity.status(200).build();
+		} else {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuário não foi encontrado");
+		}
+	}
+	
+	@PutMapping("/atualizar")
+	public ResponseEntity<Usuario> atualizarUsuario(@RequestBody Usuario usuario) {
+		return usuarioRepository.findById(usuario.getIdUsuario())
+				.map(resp -> ResponseEntity.status(200).body(usuarioRepository.save(usuario)))
+				.orElseGet(() -> {
+					throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Id não encontrado");
+				});
+	}
+	
+	@DeleteMapping("/deletar/{idUsuario}")
+	public ResponseEntity<Usuario> deletarUsuario(@PathVariable(value = "idUsuario") Long idUsuario) {
+		Optional<Usuario> usuario = usuarioRepository.findById(idUsuario);
+		
+		if (usuario.isPresent()) {
+			usuarioRepository.deleteById(idUsuario);
+			return ResponseEntity.status(200).build();
+		} else {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Id não foi encontrado");
+		}
+	}
 }
